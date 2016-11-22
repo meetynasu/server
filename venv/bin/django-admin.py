fc26cd8477ca2452692791167eb3c59556e1ad22
sergioruizdavila <sergioruizdavila@gmail.com>
2016-11-22 18:34:36 -0500

    First Ynasu BackEnd structure

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sergioruizdavila/Documents/Projects/Ynasu/server/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
